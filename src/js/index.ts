19bb5406afb2206537507d8ef460a8f26e9b3457
VadoSVad <vadimsol@yandex.ru>
2023-12-09 01:30:25 +0300

    Попытка создания сервера

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,7 +4,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World Typescript + Node.JS');
+    res.end('Hello, this is your Node.js server!');
 });
 
 server.listen(port, () => {
